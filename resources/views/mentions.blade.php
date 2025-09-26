ecc2674fa7b1b7e384dfccd7b48935831574cf8b
James Genitrini <jamesgenitrini@gmail.com>
2025-09-26 14:25:58 -0400

    mention légale

--- resources/views/mentions.blade.php
+++ resources/views/mentions.blade.php
@@ -51,14 +51,14 @@
 
         <p><strong>Éditeur du site :</strong>
             Ozæna SARL<br>
-            SIRET : 123 456 789 00012<br>
-            Adresse : 123 Rue Exemple, 75000 Paris<br>
+            SIRET : 990 958 506 00018<br>
+            Adresse : 519 A LES COREAUX, 68910 LABAROCHE<br>
             Email : <a href="mailto:contact@ozæna.com">contact@ozæna.com</a><br>
             Téléphone : +33 1 23 45 67 89
         </p>
 
         <p><strong>Directeur de la publication :</strong>
-            Jean Dupont
+            Damien PIMMEL
         </p>
 
         <p><strong>Hébergeur :</strong>
