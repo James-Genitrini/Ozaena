eb914265b329b0fa8f25a56abbfdfc38119b74e2
James Genitrini <jamesgenitrini@gmail.com>
2025-10-03 14:04:04 -0400

    Changing title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title', 'Ozæna')</title>
+    <title>@yield('title', 'Ozæna') - Ozaena.fr</title>
     @vite('resources/css/app.css')
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet">
--- resources/views/shop/home.blade.php
+++ resources/views/shop/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Accueil')
+@section('title', 'Collection 000')
 
 @push('styles')
     <style>
